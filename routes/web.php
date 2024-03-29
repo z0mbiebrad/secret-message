f4a91f4d6dff5db595415b69a595401e040cbfa1
Bradley Evans <87036647+z0mbiebrad@users.noreply.github.com>
2024-03-29 01:34:44 -0400

    Routes for creating viewing and deleting messages

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MessageController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -7,11 +8,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
 Route::middleware('auth')->group(function () {
+
+    Route::get('/write-message', [MessageController::class, 'create'])->name('message.create');
+    Route::post('/write-message', [MessageController::class, 'store'])->name('message.store');
+
+    Route::get('/read-message', [MessageController::class, 'index'])->name('message.index');
+
+    Route::get('/show-message', [MessageController::class, 'show'])->name('message.show');
+    Route::post('/show-message', [MessageController::class, 'destroy'])->name('message.destroy');
+
+    Route::get('/delete-message/{id}', [MessageController::class, 'destroy'])->name('message.destroy');
+    Route::delete('/delete-message/{id}', [MessageController::class, 'destroy'])->name('message.destroy');
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
