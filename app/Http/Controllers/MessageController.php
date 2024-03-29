6e5d2c8c085e2ef285eb9138fc95a773445eaf5e
Bradley Evans <87036647+z0mbiebrad@users.noreply.github.com>
2024-03-28 21:04:57 -0400

    outlining and setup

---
+++ app/Http/Controllers/MessageController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Message;
+use Illuminate\Http\Request;
+
+class MessageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+ 
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        dd('Triggerd?');
+        Message::create([
+           'message' => $request('message'),
+           'recipient' => $request('recipient'),
+           'decryption_key' => $request('decryption_key'),
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Message $message)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Message $message)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Message $message)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Message $message)
+    {
+        //
+    }
+}
